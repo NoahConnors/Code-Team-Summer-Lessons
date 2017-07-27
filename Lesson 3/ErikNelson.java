217adb533eb014b6c8f12fe2991656cd623ef1b7
Erik Nelson <eriknelson@mnmsa.org>
2017-07-27 18:21:53 -0500

    Add files via upload
    zis is mein file

---
+++ Lesson 3/ErikNelson.java
@@ -0,0 +1,10 @@
+class ErikNelson {
+	public static void main(String args[]){
+		boolean test = true;
+		if(test){
+			System.out.println("The test has worked.");
+		} else {
+			System.out.println("The test has not worked.");
+		}
+	}
+}
