3942eb5172bd57effb7e0025c75c50a2bead016b
Atharva Mulay <mulayatharvar@gmail.com>
2017-07-27 18:27:31 -0500

    Atharva Mulay

---
+++ Lesson 3/AtharvaMulay.java
@@ -0,0 +1,10 @@
+class AtharvaMulay {
+	public static void main(String args[]) {
+		int x = 6;
+		if(x == 7) {
+			System.out.println("You get Donuts!!!!!!");
+		} else {
+			System.out.println("No Donuts for you!!!!!!!!!!!!!!");
+		}
+	}
+}
