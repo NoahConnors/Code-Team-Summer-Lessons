ae7aceab81ced074cc74f320a5c389b7efe08b82
elliotwoolley <elliot@attributesoftware.com>
2017-07-27 18:25:11 -0500

    Add files via upload

---
+++ Lesson 3/elliotwoolley.java
@@ -0,0 +1,10 @@
+class elliotwoolley {
+	public static void main(String args[]) {
+		String x = "";
+		if(x == "") {
+			System.out.println("");
+		} else { 
+			System.out.println("not ");
+		}
+	}
+}
