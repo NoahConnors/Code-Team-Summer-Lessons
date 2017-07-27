9ca1f482440330a8cc12c318c669259281ba447f
Pusheen6389 <li.anna6389@gmail.com>
2017-07-27 18:23:13 -0500

    Add files via upload

---
+++ Lesson 3/Anna.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+	public static void main(String args[]) {
+		boolean potato = false;
+		if(potato) {
+			System.out.println("Potatoes are cool");
+		} else {
+			System.out.println(">:(");
+		}
+	}
+}
