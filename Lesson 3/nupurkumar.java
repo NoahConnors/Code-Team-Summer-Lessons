ede56df47f915e5d8abfe42e637ef5addc4678ee
Nupur Kumar <kumarnupurk@gmail.com>
2017-07-27 18:27:22 -0500

    Add files via upload

---
+++ Lesson 3/nupurkumar.java
@@ -0,0 +1,10 @@
+class nupurkumar {
+	public static void main(String args[]) {
+		int x=taco;
+		if(x == taco) {
+			System.out.println("you've been beaned");
+		} else {
+			System.out.println("darn");
+		}
+	}
+}
