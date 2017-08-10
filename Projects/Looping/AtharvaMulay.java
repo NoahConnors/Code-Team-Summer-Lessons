f5b4c5f670491cc1599a3e364f8dc6c4ace7e62e
Atharva Mulay <mulayatharvar@gmail.com>
2017-08-10 18:49:04 -0500

    sup bro

---
+++ Projects/Looping/AtharvaMulay.java
@@ -0,0 +1,9 @@
+class AtharvaMulay {
+	public static void main(String args[]) {
+		System.out.println("Welcome to the dungeon");
+		for(int x = 0; x < 20000; x++) {
+			System.out.println("Whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
+		}
+		System.out.println("You're Lucky, Gooooodbye.");
+	}
+}
